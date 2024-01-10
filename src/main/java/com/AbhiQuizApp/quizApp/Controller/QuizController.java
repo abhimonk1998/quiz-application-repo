99e18fe0d4fbcd19c745ebd6d91cad5ed07e06f1
Abhijeet Khedekar <abhijeetkhedekar996@gmail.com>
2024-01-10 15:47:29 -0700

    Removed correct answer option from result to client

--- src/main/java/com/AbhiQuizApp/quizApp/Controller/QuizController.java
+++ src/main/java/com/AbhiQuizApp/quizApp/Controller/QuizController.java
@@ -1,20 +1,22 @@
 package com.AbhiQuizApp.quizApp.Controller;
 
 import com.AbhiQuizApp.quizApp.Constants.Category;
-import com.AbhiQuizApp.quizApp.Model.Quiz;
+import com.AbhiQuizApp.quizApp.Model.QuizClientDTO;
 import com.AbhiQuizApp.quizApp.Service.QuizService;
+import org.hibernate.ObjectNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/quiz")
@@ -26,20 +28,20 @@ public class QuizController {
     @Autowired
     private QuizService quizService;
 
-    @GetMapping()
-    private ResponseEntity<Quiz> getQuiz(@RequestParam Integer quizId){
+    @GetMapping("get/{id}")
+    private ResponseEntity<QuizClientDTO> getQuiz(@PathVariable Integer id){
         try {
-            Optional<Quiz> quiz = quizService.getQuiz(quizId);
-            if(quiz.isPresent()){
-                return new ResponseEntity<>(quiz.get(), HttpStatus.OK);
+            QuizClientDTO quiz = quizService.getQuiz(id);
+            if(Objects.nonNull(quiz)) {
+                return new ResponseEntity<>(quiz, HttpStatus.OK);
             } else {
-                throw new Exception("Quiz not present");
+                throw new ObjectNotFoundException(QuizClientDTO.class,"Object not found");
             }
+
         } catch (Exception e){
             logger.error("Error occurred to fetch quiz");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
     }
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String title, @RequestParam Category category, @RequestParam int numQuestions) {
